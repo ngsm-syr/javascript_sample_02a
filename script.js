f338993c898c26e4c8a9a2a7f6a2058bb328febb
Nagashima Sayuri <106061832+ngsm-syr@users.noreply.github.com>
2025-05-30 12:38:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -16,21 +16,26 @@ btnBlue.addEventListener('click', () => {
 });
 
 // 5-2｜引数
+function changeBgColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
 const btnArgs = document.querySelectorAll('.btn-arg');
 btnArgs.forEach(btn => {
   btn.addEventListener('click', () => {
     const color = btn.getAttribute('data-color');
-    document.body.style.backgroundColor = color;
+    changeBgColor(color); // 引数として渡す
   });
 });
 
-// ランダムな16進6桁のカラーコードを生成（汎用性のある関数）
+// ６｜汎用性のある関数
+// ランダムな16進6桁のカラーコードを生成
 function randomColor() {
   const col = Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
   return '#' + col;
 }
 
-// ６｜汎用性のある関数
+// 関数の呼び出し
 const btnGeneric = document.querySelector('.btn-generic');
 btnGeneric.addEventListener('click', () => {
   document.body.style.backgroundColor = randomColor();
